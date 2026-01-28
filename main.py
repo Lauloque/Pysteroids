805d0d3e692e58939dc59fa5cb3d58ab213deaf6
Loïc 'Lauloque' Dautry <loic.dautry@gmail.com>
2026-01-28 00:38:01 -0500

    add player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 from logger import log_event
 
 def main():
@@ -16,20 +17,31 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
 
+    Player.containers = updatable, drawable
+
+    player = Player((SCREEN_WIDTH // 2), (SCREEN_HEIGHT // 2))
+
     dt = 0
 
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         updatable.update(dt)
+
         screen.fill("black")
+
+
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
---
+++ player.py
@@ -0,0 +1,49 @@
+import pygame
+from circleshape import CircleShape
+from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_TURN_SPEED, SHOT_RADIUS, PLAYER_SHOT_SPEED
+from shot import Shot
+
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            self.rotate(-dt)
+        if keys[pygame.K_RIGHT]:
+            self.rotate(dt)
+        if keys[pygame.K_UP]:
+            self.move(dt)
+        if keys[pygame.K_DOWN]:
+            self.move(-dt)
+        if keys[pygame.K_RCTRL]:
+            self.shoot()
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0,1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed = rotated_vector * PLAYER_TURN_SPEED * dt
+        self.position += rotated_with_speed
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOT_SPEED
+        
